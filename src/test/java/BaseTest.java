79fa7a1adc1e01f1fe79d7351c3fe8047c16383c
Master <master@>
2023-04-16 02:47:01 +0300

    project_name

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,15 +1,39 @@
 import com.codeborne.selenide.Configuration;
-import static com.codeborne.selenide.Browsers.FIREFOX;
-import org.junit.Before;
+import com.codeborne.selenide.FileDownloadMode;
+import com.codeborne.selenide.Selenide;
+import dev.failsafe.internal.util.Assert;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+
+
 
 
 public class BaseTest {
-    @Before
+    @BeforeTest
     public void setBrowser()
     {
+        //Загружаем веб драйвер из GIT
+        WebDriverManager.firefoxdriver().setup();
+
+
         //Запускаем тест в браузере FIREFOX
-       Configuration.browser = FIREFOX ;
+        Configuration.browser ="firefox" ;
         Configuration.browserSize = "1920x1080";
+        //Для быстрого ввода текста в поля
+        Configuration.fastSetValue = true;
+        //Запускать скрытый браузер
+        //Configuration.headless=true;
+
 
     }
 
