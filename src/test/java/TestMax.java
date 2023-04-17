eec0dbab75afdb7fad40a8a6fd090f2ed6803dd6
Master <master@>
2023-04-17 11:45:11 +0300

    project_name1

--- src/test/java/TestMax.java
+++ src/test/java/TestMax.java
@@ -39,7 +39,7 @@ import com.codeborne.selenide.Selenide;
 
 
         //Вводим наименование Бренда
-        $(byXpath("//elma-form-control/elma-type-string/div/input")).sendKeys("auto_test_a3"); // воткнуть переменную на имя бренд
+        $(byXpath("//elma-form-control/elma-type-string/div/input")).sendKeys("auto_test_a4"); // воткнуть переменную на имя бренд
 //Выбор из справочника рекламодателей
         $(byXpath("(.//*[normalize-space(text()) and normalize-space(.)='Рекламодатель'])[1]/following::button[1]")).click();
 // Выбираем из справочника  бренд
@@ -55,9 +55,11 @@ import com.codeborne.selenide.Selenide;
 // Нажимаем сохранить
         $(byXpath("//*/text()[normalize-space(.)='Сохранить']/parent::*")).shouldBe(enabled,ofSeconds(5)).click();
 //Ждем 5 секунд м нажимаем на созданный элемент
-        $(byText("auto_test_a3")).shouldBe(enabled,ofSeconds(5));
-        $(byText("auto_test_a3")).click();
-        $(byText(";ждаТь")).shouldBe(enabled,ofSeconds(15));
+        $(byText("auto_test_a4")).shouldBe(enabled,ofSeconds(5));
+        $(byText("auto_test_a4")).click();
+        //$(byText(";ждаТь")).shouldBe(enabled,ofSeconds(15));
+
+
 
 
 
